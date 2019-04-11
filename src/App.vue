a937755dea008c09380f350250b822c154705429
youjunjie <957028613@qq.com>
2019-04-11 19:11:35 +0800

    testing flip

--- src/App.vue
+++ src/App.vue
@@ -4,6 +4,7 @@
     <base-button :plain="true" @CLICK_BTN="clickDialogBtn" disabled>不可用按钮</base-button>
     <base-button @CLICK_BTN="clickDialogBtn">触发dialog按钮</base-button>
     <base-button @CLICK_BTN="clickAlertBtn">触发alert按钮</base-button>
+    <base-button @CLICK_BTN="clickFlipBtn">触发flip按钮</base-button>
     <base-alert
       :show-alert="showAlert"
       :has-icon="true"
@@ -28,6 +29,11 @@
     <Marquee :content="arr">
       <span class="marquee-content" v-for="(item, index) in arr" :key="index">{{item}}</span>
     </Marquee>
+    <div class="dd" :class="{flip}">
+      https://codepen.io/shayhowe/pen/Fvjnf
+      <div class="bb face"></div>
+      <div class="bb back"></div>
+    </div>
   </div>
 </template>
 
@@ -36,6 +42,7 @@ import BaseButton from './components/BaseButton';
 import BaseDialog from './components/BaseDialog';
 import BaseAlert from './components/BaseAlert';
 import Marquee from './components/Marquee';
+import { setTimeout } from 'timers';
 
 export default {
   name: 'App',
@@ -49,18 +56,21 @@ export default {
     return {
       showDialog: false,
       showAlert: false,
-      arr: []
+      arr: [],
+      flip: false,
+      flipEnd: false,
+      text: '1111'
     };
   },
   mounted() {
     this.arr = [
-      '手机用户:151****013获得二等奖',
-      '科比:获得一个篮球',
-      '网友:获得现金5元',
-      '网友:获得现金5元',
-      '网友:获得现金5元',
-      '网友:获得现金5元',
-      '网友:获得现金5元'
+      '111111111111111',
+      '222222222222222',
+      '333333333333333',
+      '444444444444444',
+      '555555555555555',
+      '666666666666666',
+      '777777777777777'
     ];
   },
   methods: {
@@ -79,6 +89,13 @@ export default {
     closeAlert() {
       console.log('close alert');
       this.showAlert = false;
+    },
+    clickFlipBtn() {
+      console.log('flip');
+      this.flip = true;
+      setTimeout(() => {
+        this.text = '22222';
+      }, 1000);
     }
   }
 };
@@ -97,6 +114,33 @@ export default {
   position: relative;
   .marquee-content {
     margin-left: 20px;
+  }
+  .dd {
+    position: absolute;
+    width: 200px;
+    height: 200px;
+    top: 50%;
+    left: 50%;
+    margin-left: -75px;
+    transition: all 1s linear;
+    & > .bb {
+      width: 200px;
+      height: 200px;
+      position: absolute;
+      top: 0;
+      left: 0;
+    }
+    .face {
+      @include set-image('./assets/images/1.jpg');
+    }
+    .back {
+      @include set-image('./assets/images/2.jpg');
+      z-index: 1;
+    }
+    &.flip {
+      transform: rotateY(180deg);
+      backface-visibility: hidden;
+    }
   }
 }
 </style>

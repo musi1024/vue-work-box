189a605948c18f1aa8a3034707846eb704848b04
youjunjie <957028613@qq.com>
2019-04-27 23:28:38 +0800

    .

--- src/main.js
+++ src/main.js
@@ -8,10 +8,7 @@ import eventBus from './utils/eventBus';
 Vue.use(eventBus);
 
 Vue.config.productionTip = false;
-const wechat = new window.Wechat('wx8d4dcbcc3d3f1cb9');
-Vue.prototype.$wechat = wechat;
-// 初始化wechat
-wechat.config();
+
 new Vue({
   router,
   store,

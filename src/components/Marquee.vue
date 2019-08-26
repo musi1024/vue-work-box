ad1c427eb5b635f2d1f11399636f488035cd4d4a
youjunjie <957028613@qq.com>
2019-08-26 14:34:59 +0800

    modify marquee

--- src/components/Marquee.vue
+++ src/components/Marquee.vue
@@ -1,16 +1,11 @@
 <template>
-  <div ref="wrap" id="Marquee">
-    <div
-      ref="content"
-      id="Marquee-content"
-      :class="[animationClass, isHorizontal ? 'horizontal' : '']"
-      :style="contentStyle"
-    >
-      <slot></slot>
+  <section ref="wrap" id="Marquee">
+    <div ref="content" class="marquee-content" :class="[animationClass]" :style="contentStyle">
+      <div class="marquee-item" v-for="i in content" :key="i">{{i}}</div>
     </div>
-    
-  </div>
+  </section>
 </template>
+
 <script>
 export default {
   name: 'Marquee',
@@ -18,40 +13,27 @@ export default {
     content: {
       default: ''
     },
-    isSeamless: {
-      type: Boolean,
-      default: false
-    },
-    isHorizontal: {
-      type: Boolean,
-      default: false
-    },
     delay: {
       type: Number,
       default: 0.5
     },
     speed: {
       type: Number,
-      default: 50
+      default: 60
     }
   },
-  mounted() {},
   data() {
     return {
-      wrapWidth: 0, //父盒子宽度
-      firstRound: true, //判断是否
-      duration: 0, //css3一次动画需要的时间
-      offsetWidth: 0, //子盒子的宽度,
-      wrapHeight: 0,
-      offsetHeight: 0,
-      animationClass: '' //添加animate动画
+      wrapWidth: 0, //父元素宽度
+      offsetWidth: 0, // 内容宽度,
+      duration: 0, // 一次动画的时间
+      animationClass: '' // animattion
     };
   },
   computed: {
     contentStyle() {
       return {
-        paddingLeft: (this.isHorizontal ? this.wrapWidth : 0) + 'px',
-        paddingTop: (this.isHorizontal ? 0 : this.wrapHeight) + 'px',
+        paddingLeft: this.wrapWidth + 'px',
         animationDelay: this.delay + 's',
         animationDuration: this.duration + 's'
       };
@@ -61,21 +43,15 @@ export default {
     content: {
       // 监听到有内容,计算宽度,并计算时间,添加动画
       handler() {
+        this.wrapWidth = 0;
         this.$nextTick(() => {
           const { wrap, content } = this.$refs;
           const wrapWidth = wrap.getBoundingClientRect().width;
           const offsetWidth = content.getBoundingClientRect().width;
-          const wrapHeight = wrap.getBoundingClientRect().height;
-          const offsetHeight = content.getBoundingClientRect().height;
           this.wrapWidth = wrapWidth;
           this.offsetWidth = offsetWidth;
-          this.wrapHeight = wrapHeight;
-          this.offsetHeight = offsetHeight;
-          this.duration =
-            (this.isHorizontal ? offsetWidth : offsetHeight) / this.speed;
-          this.animationClass = this.isHorizontal
-            ? 'horizontal-infinite'
-            : 'vertical-infinite';
+          this.duration = offsetWidth / this.speed;
+          this.animationClass = 'horizontal-infinite';
         });
       }
     }
@@ -83,46 +59,41 @@ export default {
   methods: {}
 };
 </script>
-<style lang="scss" scoped>
+
+<style lang="scss">
 #Marquee {
-  width: 100%;
-  height: 24px;
-  line-height: 24px;
-  overflow: hidden;
-  position: relative;
-  background: #000000;
-  color: #eeeeee;
-  font-size: 12px;
   position: relative;
   padding: 0;
+  width: 100%;
+  height: px(76);
+  line-height: px(76);
+  font-size: px(46);
+  color: rgb(255, 255, 255);
+  background-color: black;
+  overflow: hidden;
 
-  &-content {
+  .marquee-content {
     @include flex();
     flex-direction: column;
-    &.horizontal {
-      position: absolute;
-      white-space: nowrap;
-      flex-direction: row;
-    }
+    position: absolute;
+    white-space: nowrap;
+    flex-direction: row;
+    -webkit-backface-visibility: hidden;
+    backface-visibility: hidden;
+    z-index: 1;
+  }
+  .marquee-item {
+    margin-left: px(70);
   }
 
   .horizontal-infinite {
     animation: horizontal-infinite linear infinite;
-  }
-  .vertical-infinite {
-    animation: vertical-infinite linear infinite;
   }
 }
 
 @keyframes horizontal-infinite {
   to {
     transform: translate3d(-100%, 0, 0);
-  }
-}
-
-@keyframes vertical-infinite {
-  to {
-    transform: translate3d(0, -100%, 0);
   }
 }
 </style>

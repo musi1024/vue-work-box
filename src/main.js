71ae011623ff8db9a11cbdf9b40bc24f790fc75f
youjunjie <957028613@qq.com>
2019-09-12 15:32:03 +0800

    fix: vconsole

--- src/main.js
+++ src/main.js
@@ -7,13 +7,16 @@ import App from './App.vue';
 import router from './router';
 import store from './store';
 
+if (process.env.VUE_APP_VCONSOLE === 'true') {
+  if (!/localhost/.test(window.location.origin)) {
+    const VConsole = require('vconsole');
+    new VConsole();
+  }
+}
+
 /* 动态创建组件 */
 import alert from './plugins/alert';
 Vue.use(alert);
-
-/* 禁止 touchmove */
-import preventScroll from './utils/Browser/preventScroll';
-preventScroll();
 
 Vue.config.productionTip = false;
 

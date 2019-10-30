8e8873fc7960e34e8de31bd340a9833ba59e7b35
youjunjie <jkmusi1024@gmail.com>
2019-10-30 17:46:31 +0800

    feat: store showpopup

--- src/store.js
+++ src/store.js
@@ -3,8 +3,35 @@ import Vuex from 'vuex';
 
 Vue.use(Vuex);
 
-export default new Vuex.Store({
-  state: {},
-  mutations: {},
+const store = new Vuex.Store({
+  state: {
+    popup: {}
+  },
+  getters: {},
+  mutations: {
+    showPopup(state, payload) {
+      if (Object.prototype.toString.call(payload.props) === '[object Object]') {
+        Object.assign(state.popup[payload.type], payload.props);
+      } else {
+        state.popup[payload.type] = payload.props;
+      }
+    }
+  },
   actions: {}
 });
+
+function showPopup(type, props) {
+  return store.commit('showPopup', { type, props });
+}
+const popup = {
+  install() {
+    Object.defineProperty(Vue.prototype, '$popup', {
+      get: function get() {
+        return showPopup;
+      }
+    });
+  }
+};
+Vue.use(popup);
+
+export default store;

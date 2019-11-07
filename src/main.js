b2d34e8c8734400c842e94450fb74e872162a5bc
youjunjie <jkmusi1024@gmail.com>
2019-11-07 16:54:43 +0800

    use vconsole plugin

--- src/main.js
+++ src/main.js
@@ -9,12 +9,7 @@ import store from './store';
 import 'core-js/fn/object/assign';
 import 'core-js/fn/string/includes';
 
-if (process.env.VUE_APP_VCONSOLE !== 'false') {
-  if (!/localhost/.test(window.location.origin)) {
-    const VConsole = require('vconsole');
-    new VConsole();
-  }
-}
+import './rpf/vue/vconsole';
 
 /* 动态创建全局组件 */
 import create from './plugins/create';

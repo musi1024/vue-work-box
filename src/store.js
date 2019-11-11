28b461d42892c8d6f3d1af2cd0771e90319b2dc3
youjunjie <jkmusi1024@gmail.com>
2019-11-11 10:19:31 +0800

    modify store

--- src/store.js
+++ src/store.js
@@ -3,27 +3,38 @@ import Vuex from 'vuex';
 
 Vue.use(Vuex);
 
-const store = new Vuex.Store({
-  state: {
-    popup: { test: { show: false } }
-  },
-  getters: {},
-  mutations: {
-    showPopup(state, payload) {
-      if (Object.prototype.toString.call(payload.props) === '[object Object]') {
-        state.popup[payload.type] = {
-          ...state.popup[payload.type],
-          ...payload.props
-        };
-      } else {
-        state.popup[payload.type] = payload.props;
-      }
-    }
-  },
-  actions: {}
+const state = {
+  popup: {
+    test: { show: false, type: null },
+    test2: { show: false, type: null }
+  }
+};
+
+const getters = {};
+Object.keys(state.popup).map(i => {
+  getters[i] = state => {
+    return state.popup[i];
+  };
 });
 
-store.getters = { ...store.getters, ...store.state.popup };
+const mutations = {
+  showPopup(state, payload) {
+    if (Object.prototype.toString.call(payload.props) === '[object Object]') {
+      state.popup[payload.type] = {
+        ...state.popup[payload.type],
+        ...payload.props
+      };
+    } else {
+      state.popup[payload.type] = payload.props;
+    }
+  }
+};
+
+const store = new Vuex.Store({
+  state,
+  getters,
+  mutations
+});
 
 function showPopup(type, props) {
   store.commit('showPopup', { type, props });

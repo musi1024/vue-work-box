8896ba58114768534365e99718655396d09ba037
youjunjie <jkmusi1024@gmail.com>
2019-12-05 10:26:39 +0800

    clear

--- src/utils/Others/scrollSmoothTo.js
+++ src/utils/Others/scrollSmoothTo.js
@@ -11,13 +11,9 @@ function scrollSmoothTo(el = window, position = 0) {
       return setTimeout(callback, 10);
     };
   }
-  /* 当前滚动高度位置 */
   let scrollTop = el.scrollTop;
-  /* 滚动step方法 */
   const step = () => {
-    /* 距离目标滚动距离 */
     let distance = position - scrollTop;
-    /* 每次目标滚动位置 */
     scrollTop = scrollTop + distance / 5;
     if (Math.abs(distance) < 1) {
       el.scrollTo(0, position);

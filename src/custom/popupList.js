bfb9f47419159778fe8433eb9071ee6aae192be9
youjunjie <jkmusi1024@gmail.com>
2019-12-20 15:10:50 +0800

    refactor: popup list

--- src/custom/popupList.js
+++ src/custom/popupList.js
@@ -1,9 +1,3 @@
-import Test1 from '@/components/Popup/Test1';
-import Test2 from '@/components/Popup/Test2';
-
-const popupList = {
-  test1: Test1,
-  test2: Test2
-};
+const popupList = {};
 
 export default popupList;

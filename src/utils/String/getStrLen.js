addaddf9b0ba99cb94572421c5ccb82722d48deb
youjunjie <jkmusi1024@gmail.com>
2020-02-17 11:12:17 +0800

    feat: getStrLen

---
+++ src/utils/String/getStrLen.js
@@ -0,0 +1,11 @@
+export default function getStrLen(str) {
+  let len = 0;
+  for (let i = 0; i < str.length; i++) {
+    if (str.charCodeAt(i) > 127 || str.charCodeAt(i) == 94) {
+      len += 2;
+    } else {
+      len++;
+    }
+  }
+  return len;
+}

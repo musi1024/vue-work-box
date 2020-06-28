f422f034655222c0531d2d959fb7cd57f56a8fde
youjunjie <jkmusi1024@gmail.com>
2020-06-28 17:05:01 +0800

    feat: rpf

--- src/rpf/un/getQuery.js
+++
@@ -1,7 +0,0 @@
-import qs from 'qs';
-
-function getQuery() {
-  return qs.parse(window.location.search.slice(1));
-}
-
-export default getQuery;
--- src/rpf/un/isAndroid.js
+++
@@ -1,5 +0,0 @@
-function isAndroid() {
-  return /Android /i.test(navigator.userAgent);
-}
-
-export default isAndroid;
